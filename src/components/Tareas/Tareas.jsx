35683e4fd91444e87c6f980cc01e6fde465aa6f0
Nyland-Martinez <martinezhaidee95@gmail.com>
2023-12-29 13:28:00 -0300

    commit

---
+++ src/components/Tareas/Tareas.jsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from "react";
+import styles from './Tareas.module.css';
+
+const TodoList = () => {
+    
+    const [tasks, setTasks] = useState([]);
+    const [newTask, setNewTask] = useState("");
+
+    useEffect(() => {
+        let data = localStorage.getItem("tasks");
+        if (data) {
+            setTasks(JSON.parse(data));
+        }
+    }, []);
+    
+    useEffect(() => {
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    }, [tasks]);
+
+    const AddTask = () => {
+        if (newTask.trim() !== "") {
+        setTasks([...tasks, { text: newTask.trim(), checked: false }]);
+        setNewTask("");
+        }
+    };
+
+    const DeleteTask = (index) => {
+        const newTask = [...tasks];
+        newTask.splice(index, 1);
+        setTasks(newTask);
+    };
+
+    const ToggleTask = (index) => {
+        const newTask = [...tasks];
+        newTask[index].checked = !newTask[index].checked;
+        setTasks(newTask);
+    };
+
+return (
+    <div className= {styles.todo}>
+        <h1>Lista de Tareas</h1>
+        <input className={styles.inp}
+        type="text"
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
+        />
+        <button
+            className={styles.btn1}
+            onClick={AddTask}
+        >
+            Agregar
+        </button>
+        <div>
+            {tasks.map((task, index) => (
+            <div className={styles.container}
+                key={index}
+                task={task}
+            >
+            <div className={styles.flex2}>
+                <span
+                style={{
+                    marginRight: "10px",
+                    textDecoration: task.checked ? "line-through" : "none",
+                }}
+                >
+                {task.text}
+                </span>
+                <input
+                type="checkbox"
+                checked={task.checked}
+                onChange={() => ToggleTask(index)}
+                />
+            </div>
+            <div className={styles.btn}>
+                <button
+                className={styles.btn2}
+                onClick={() => DeleteTask(index)}
+                >
+                Eliminar
+                </button>
+            </div>
+        </div>
+        ))}
+        </div>
+    </div>
+    );
+};
+
+export default TodoList;
